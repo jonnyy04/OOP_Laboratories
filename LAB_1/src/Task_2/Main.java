545304fd6bf36aa78d4138b404fe9579da4f6d66
jonny04 <ionvornicescu4@gmail.com>
2024-10-28 18:37:14 +0200

    Made the task_2 to handle multiple file

--- LAB_1/src/Task_2/Main.java
+++ LAB_1/src/Task_2/Main.java
@@ -3,19 +3,22 @@ package Task_2;
 public class Main {
     public static void main(String[] args) {
         if (args.length < 1) {
-            System.out.println("Please provide the path to the text file as a command-line argument.");
+            System.out.println("Please provide the paths to the text files as command-line arguments.");
             return;
         }
 
-        String filePath = args[0];
-        System.out.println(filePath);
-        String text = FileReader.readFileIntoString(filePath);
+        // Loop through each file path provided in the command-line arguments
+        for (String filePath : args) {
+            System.out.println("Reading file: " + filePath);
 
-        if (!text.isEmpty()) {
-            TextData textData = new TextData(filePath, text);
-            System.out.println(textData);
-        } else {
-            System.out.println("The file is empty or couldn't be read.");
+            String text = FileReader.readFileIntoString(filePath);
+
+            if (text != null && !text.isEmpty()) {
+                TextData textData = new TextData(filePath, text);
+                System.out.println(textData);
+            } else {
+                System.out.println("The file at " + filePath + " is empty or couldn't be read.");
+            }
         }
     }
 }

2b174ce4f17a49c19d7ca0de3d366b0217033a0b
jonny04 <ionvornicescu4@gmail.com>
2024-11-03 00:06:46 +0200

    Task 1 done

---
+++ LAB_2/src/LAB_2/Task_1/Main.java
@@ -0,0 +1,36 @@
+package LAB_2.Task_1;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create a regular Coffee instance
+        Coffee regularCoffee = new Coffee(Intensity.NORMAL);
+        System.out.println("Coffee name: " + regularCoffee.getName());
+        System.out.println("Intensity: " + regularCoffee.getCoffeeIntensity());
+
+        // Create a Cappuccino instance
+        Cappuccino cappuccino = new Cappuccino(Intensity.STRONG, 150);
+        System.out.println("\nCoffee name: " + cappuccino.getName());
+        System.out.println("Intensity: " + cappuccino.getCoffeeIntensity());
+        System.out.println("Milk amount (ml): " + cappuccino.getMlOfMilk());
+
+        // Create an Americano instance
+        Americano americano = new Americano(Intensity.LIGHT, 200);
+        System.out.println("\nCoffee name: " + americano.getName());
+        System.out.println("Intensity: " + americano.getCoffeeIntensity());
+        System.out.println("Water amount (ml): " + americano.getMlOfWater());
+
+        // Create a SyrupCappuccino instance
+        SyrupCappuccino syrupCappuccino = new SyrupCappuccino(Intensity.NORMAL, 150, SyrupType.VANILLA);
+        System.out.println("\nCoffee name: " + syrupCappuccino.getCoffee());
+        System.out.println("Intensity: " + syrupCappuccino.getCoffeeIntensity());
+        System.out.println("Milk amount (ml): " + syrupCappuccino.getMlOfMilk());
+        System.out.println("Syrup flavor: " + syrupCappuccino.getSyrup());
+
+        // Create a PumpkinSpiceLatte instance
+        PumpkinSpiceLatte pumpkinSpiceLatte = new PumpkinSpiceLatte(Intensity.STRONG, 150, 30);
+        System.out.println("\nCoffee name: " + pumpkinSpiceLatte.getName());
+        System.out.println("Intensity: " + pumpkinSpiceLatte.getCoffeeIntensity());
+        System.out.println("Milk amount (ml): " + pumpkinSpiceLatte.getMlOfMilk());
+        System.out.println("Pumpkin spice amount (mg): " + pumpkinSpiceLatte.getMgOfPumpkinSpice());
+    }
+}
---
+++ LAB_2/src/LAB_2/Task_1/SyrupCappuccino.java
@@ -0,0 +1,23 @@
+package LAB_2.Task_1;
+
+public class SyrupCappuccino extends Cappuccino {
+    private SyrupType syrup;
+    private final String coffee = "SyrupCappuccino";
+
+    public SyrupCappuccino(Intensity intensityCoffee,int mlOfWater, SyrupType  syrup) {
+        super(intensityCoffee, mlOfWater);
+        this.syrup = syrup;
+    }
+
+    public SyrupType getSyrup() {
+        return syrup;
+    }
+
+    public void setSyrup(SyrupType syrup) {
+        this.syrup = syrup;
+    }
+
+    public String getCoffee() {
+        return coffee;
+    }
+}

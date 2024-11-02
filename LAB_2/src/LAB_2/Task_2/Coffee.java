636d25ea9f54f04f50c874a7008076fb7915f558
jonny04 <ionvornicescu4@gmail.com>
2024-11-03 00:41:42 +0200

    Task_2 done

---
+++ LAB_2/src/LAB_2/Task_2/Coffee.java
@@ -0,0 +1,34 @@
+package LAB_2.Task_2;
+
+public class Coffee {
+     private Intensity coffeeIntensity;
+     private final String name = "Coffee";
+
+     Coffee(Intensity coffeeIntensity) {
+         this.coffeeIntensity = coffeeIntensity;
+     }
+     public void setCoffeeIntensity(Intensity coffeeIntensity) {
+         this.coffeeIntensity = coffeeIntensity;
+     }
+
+     public Intensity getCoffeeIntensity() {
+         return coffeeIntensity;
+     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void PrintDetails(){
+         System.out.println("\nName: " + getName());
+         System.out.println("Coffee Intensity: " + coffeeIntensity);
+    }
+}
+
+enum Intensity {
+    LIGHT, NORMAL, STRONG
+}
+
+enum SyrupType {
+    MACADAMIA, VANILLA, COCONUT, CARAMEL, CHOCOLATE, POPCORN
+}
---
+++ LAB_2/src/LAB_2/Task_2/PumpkinSpiceLatte.java
@@ -0,0 +1,30 @@
+package LAB_2.Task_2;
+
+public class PumpkinSpiceLatte extends Cappuccino {
+    private int mgOfPumpkinSpice;
+    private final String name = "Pumpkin Spice Latte";
+
+    PumpkinSpiceLatte(Intensity intensityOfCoffee, int mlOfMilk, int mgOfPumpkinSpice) {
+        super(intensityOfCoffee, mlOfMilk);
+        this.mgOfPumpkinSpice = mgOfPumpkinSpice;
+    }
+
+    public int getMgOfPumpkinSpice() {
+        return mgOfPumpkinSpice;
+    }
+
+    public void setMgOfPumpkinSpice(int mg) {
+        this.mgOfPumpkinSpice = mg;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void PrintDetails(){
+        super.PrintDetails();
+        System.out.println("mgOfPumpkinSpice: " + mgOfPumpkinSpice);
+    }
+}
+
